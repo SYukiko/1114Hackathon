41f14f3d26b02337633c2addb89710cf58c9c9e4
Yukiko <ys.m.e@live.jp>
2020-11-14 17:51:25 +0900

    ugoku yatsu add

--- study_room/ios/Flutter/flutter_export_environment.sh
+++ study_room/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/yukiko/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/yukiko/AndroidStudioProjects/study_room"
-export "FLUTTER_TARGET=/Users/yukiko/AndroidStudioProjects/study_room/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/yukiko/AndroidStudioProjects/app_study/study_room"
+export "FLUTTER_TARGET=/Users/yukiko/AndroidStudioProjects/app_study/study_room/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
